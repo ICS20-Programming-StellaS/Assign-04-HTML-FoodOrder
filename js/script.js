5840a45bb46b52ba0e42676fb74dcf0837bc02ce
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-05-13 15:05:05 +0000

    edited

--- js/script.js
+++ js/script.js
@@ -6,74 +6,77 @@
 
 "use strict"
 
-/**
- * This function displays the discount depending on age and day of the week.
- */
-function displayCost() {
-// declare constants
+function orderSent() {
+  //setting a constant for tax
   const SNACK_PRICE = 3.78;
   const MED_PRICE = 5.27;
   const LAR_PRICE = 7.69;
   const PROT_PRICE = 4.00;
-  const TOP_PRICE = 5.00;
-  const PLAST_PRICE = 3.00;
-  const SWE_PRICE = 4.00;
+  const FRUIT_PRICE = 4.00;
   const HST = 0.13;
   
-// initialize variables
-  let sizePrice = 0
-  let extraPrice = 0
-	
-	// get age and day of the week	
-	let sizeSelection = document.getElementById('size').value;
-  let size = sizeSelection.options[sizeSelection.selectedIndex].value;
+  //initializing variables
+  let baseSmoothCost = 0;
   
-  let flavourSelection = document.getElementById('flavour').value;
-  let flavour = flavourSelection.options[flavourSelection.selectedIndex].value;
+  //declaring variables for base and sides
+  let Size = document.getElementById("size").value;
+  let Flavour = document.getElementById("flavour").value;
+  let Extra = document.getElementById("extra").value;
+
+  //determining the number of checkboxes checked (number of toppings selected) - method found at https://www.techiedelight.com/count-number-check-boxes-javascript/
+  let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+  let numExtra = checkboxes.length;
   
-  let extraSelection = document.getElementById('extra_a').value;
-  let extra = extraSelection.options[extraSelection.selectedIndex].value;
-
-  let extraSelection = document.getElementById('extra_b').value;
-  let extra = extraSelection.options[extraSelection.selectedIndex].value;
-
-  let extraSelection = document.getElementById('extra_c').value;
-  let extra = extraSelection.options[extraSelection.selectedIndex].value;
-
-  let extraSelection = document.getElementById('extra_d').value;
-  let extra = extraSelection.options[extraSelection.selectedIndex].value;
-
+  //determining the cost of the base using compound if/then statements
   
-  //find out price
-  if (size == "Snack")  {
-		sizePrice = Snack_PRICE 
-	}
-	else if (size == "Medium") {
-		sizePrice = MED_PRICE
-	}
-	else if (size == "large") {
-		sizePrice = LAR_PRICE
-	}
-  
-  // extras
-   if (extra == "protein") {
-		extraPrice = PROT_PRICE
-	}
-	else if (extra == "fruit") {
-		extraPrice = TOP_PRICE
-	}
-	else if (extra == "straw") {
-		extraPrice = PLAST_PRICE
-	}
-  else if (extra == "sweetner") {
-		extraPrice = SWE_PRICE
-	}
+  //if the doughnut flavour is vanilla and the size is single
+  if ((size == "")) {
 
-  // Totals
-	let subtotal = + extraPrice + sizePrice
-  let tax = subtotal * HST
-  let total = subtotal + tax
-    	
-  	// display the cost
-  document.getElementById("display-cost").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  }
+  else if ((Flavour == "Banana") && (Size == "snack")) {
+    baseSmoothCost = SNACK_PRICE;
+  }
+    
+  //if the doughnut flavour is vanilla and the size is single
+  else if ((Flavour == "Berry") && (Size == "snack")) {
+    baseSmoothCost = SNACK_PRICE;
+  }
+    
+  //if the doughnut flavour is vanilla and the size is single
+  else if ((Flavour == "Tropical") && (Size == "snack")) {
+    baseSmoothCost = SNACK_PRICE;
+  }
+    
+  //if the doughnut flavour is vanilla and the size is single
+  else if ((Flavour == "Green") && (Size == "snack")) {
+    baseSmoothCost = SNACK_PRICE;
+  }
+
+  else if ((Flavour == "Banana") && (Size == "medium")) {
+    baseSmoothCost = MED_PRICE;
+  }
+
+  else if ((Flavour == "Berry") && (Size == "medium")) {
+    baseSmoothCost = MED_PRICE;
+  }
+    
+  else if ((Flavour == "Tropical") && (Size == "medium")) {
+    baseSmoothCost = MED_PRICE;
+  }
+
+  else if ((Flavour == "Green") && (Size == "medium")) {
+    baseSmoothCost = MED_PRICE;
+  }
+
+  else if (Size == "large") {
+    baseSmoothCost = LAR_PRICE;
+  }
+  
+  //calculating the subtotal, tax and total
+  let subtotal = baseSmoothCost + costExtra;
+  let tax = subtotal * HST;
+  let total = subtotal + tax;
+
+  //displaying the totals to the screen
+  document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
 }
