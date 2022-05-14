609195e2b9d0b6e5db159fd7b4fe2d0df2b69701
Stella Silvaroli <99274582+Stella-Silvaroli06@users.noreply.github.com>
2022-05-14 05:06:09 +0000

    completed Assign-04-HTML-FoodOrder

--- js/script.js
+++ js/script.js
@@ -6,77 +6,78 @@
 
 "use strict"
 
-function orderSent() {
+function calcOrder() {
   //setting a constant for tax
   const SNACK_PRICE = 3.78;
   const MED_PRICE = 5.27;
-  const LAR_PRICE = 7.69;
-  const PROT_PRICE = 4.00;
+  const REG_PRICE = 7.69;
+  const PROT_PRICE = 5.00;
   const FRUIT_PRICE = 4.00;
+  const SWEE_PRICE = 3.00;
+  const SCONE_PRICE = 2.67
+  const BITES_PRICE = 2.98
+  const CUP_PRICE = 1.79
+  const COOKIE_PRICE = 2.56
   const HST = 0.13;
-  
-  //initializing variables
-  let baseSmoothCost = 0;
+
+  	// initialize variables
+  let sizePrice = 0
+  let extraPrice = 0
+  let sidePrice = 0
   
   //declaring variables for base and sides
-  let Size = document.getElementById("size").value;
-  let Flavour = document.getElementById("flavour").value;
-  let Extra = document.getElementById("extra").value;
+  let size = document.getElementById("size").value;
+  let extra = document.getElementById("extra").value;
+  let side = document.getElementById("side").value;
 
-  //determining the number of checkboxes checked (number of toppings selected) - method found at https://www.techiedelight.com/count-number-check-boxes-javascript/
-  let checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
-  let numExtra = checkboxes.length;
+  // smoothie size
+  if (size == "snack")  {
+		sizePrice = SNACK_PRICE 
+	}
+	else if (size == "medium") {
+		sizePrice = MED_PRICE
+	}
+	else if (size == "regular") {
+		sizePrice = REG_PRICE
+	}
   
-  //determining the cost of the base using compound if/then statements
-  
-  //if the doughnut flavour is vanilla and the size is single
-  if ((size == "")) {
-
-  }
-  else if ((Flavour == "Banana") && (Size == "snack")) {
-    baseSmoothCost = SNACK_PRICE;
-  }
-    
-  //if the doughnut flavour is vanilla and the size is single
-  else if ((Flavour == "Berry") && (Size == "snack")) {
-    baseSmoothCost = SNACK_PRICE;
+  // smoothie extras
+   if (extra == "protein") {
+		extraPrice = PROT_PRICE
+	}
+     
+	else if (extra == "fruit") {
+		extraPrice = FRUIT_PRICE
   }
     
-  //if the doughnut flavour is vanilla and the size is single
-  else if ((Flavour == "Tropical") && (Size == "snack")) {
-    baseSmoothCost = SNACK_PRICE;
+	else if (extra == "sweetener") {
+		extraPrice = SWEE_PRICE
+	}
+
+  // smoothie sides
+   if (side == "scone") {
+		sidePrice = SCONE_PRICE
+	}
+     
+	else if (side == "bites") {
+		sidePrice = BITES_PRICE
   }
     
-  //if the doughnut flavour is vanilla and the size is single
-  else if ((Flavour == "Green") && (Size == "snack")) {
-    baseSmoothCost = SNACK_PRICE;
-  }
+	else if (side == "cup") {
+		sidePrice = CUP_PRICE
+	}
 
-  else if ((Flavour == "Banana") && (Size == "medium")) {
-    baseSmoothCost = MED_PRICE;
-  }
-
-  else if ((Flavour == "Berry") && (Size == "medium")) {
-    baseSmoothCost = MED_PRICE;
-  }
-    
-  else if ((Flavour == "Tropical") && (Size == "medium")) {
-    baseSmoothCost = MED_PRICE;
-  }
-
-  else if ((Flavour == "Green") && (Size == "medium")) {
-    baseSmoothCost = MED_PRICE;
-  }
-
-  else if (Size == "large") {
-    baseSmoothCost = LAR_PRICE;
+	else if (side == "cookie") {
+		sidePrice = COOKIE_PRICE
   }
   
-  //calculating the subtotal, tax and total
-  let subtotal = baseSmoothCost + costExtra;
-  let tax = subtotal * HST;
-  let total = subtotal + tax;
+
+  // Totals
+	let subtotal = sizePrice + extraPrice + sidePrice
+  let tax = subtotal * HST
+  let total = subtotal + tax
 
   //displaying the totals to the screen
-  document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + ".<br>The amount of HST added is $" + tax.toFixed(2) + ".<br>Your total is $" + total.toFixed(2) + ".";
+  document.getElementById("results").innerHTML = "Your subtotal is $" + subtotal.toFixed(2) + "<br>The amount of HST added is $" + tax.toFixed(2) +"<br>Your total is $" + total.toFixed(2)
 }
+
